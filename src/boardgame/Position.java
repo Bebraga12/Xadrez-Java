9285d7d1e1bcb1cb7133d4e706c4ce4c14edd018
Bernardo Braga <Bebraga12@gmail.com>
2023-12-17 18:21:57 -0300

    Tabuleiro printado

--- src/boardgame/Position.java
+++ src/boardgame/Position.java
@@ -1,33 +1,33 @@
 package boardgame;
 
 public class Position {
-    private int row;
-    private int column;
 
-    public Position(int row, int column) {
-        this.row = row;
-        this.column = column;
-    }
+	private int row;
+	private int column;
+	
+	public Position(int row, int column) {
+		this.row = row;
+		this.column = column;
+	}
 
+	public int getRow() {
+		return row;
+	}
 
-    public int getRow() {
-        return this.row;
-    }
+	public void setRow(int row) {
+		this.row = row;
+	}
 
-    public void setRow(int row) {
-        this.row = row;
-    }
+	public int getColumn() {
+		return column;
+	}
 
-    public int getColumn() {
-        return this.column;
-    }
-
-    public void setColumn(int column) {
-        this.column = column;
-    }
-
-    @Override
-    public String toString(){
-        return row + ", " + column;
-    }
+	public void setColumn(int column) {
+		this.column = column;
+	}
+	
+	@Override
+	public String toString() {
+		return row + ", " + column;
+	}
 }
