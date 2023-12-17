2b11c020c69cf8efa68b232cb102fba9257c8ac5
Bernardo Braga <Bebraga12@gmail.com>
2023-12-17 15:25:53 -0300

    tabuleiro

---
+++ src/tabuleiro/position.java
@@ -0,0 +1,32 @@
+package tabuleiro;
+
+public class position {
+    private int linha;
+    private int coluna;
+
+    public position(int linha, int coluna) {
+        this.linha = linha;
+        this.coluna = coluna;
+    }
+
+    public int getLinha() {
+        return this.linha;
+    }
+
+    public void setLinha(int linha) {
+        this.linha = linha;
+    }
+
+    public int getColuna() {
+        return this.coluna;
+    }
+
+    public void setColuna(int coluna) {
+        this.coluna = coluna;
+    }
+
+    @Override
+    public String ToString(){
+        return linha + ", " + coluna;
+    }
+}
