5779b80ff9475609c49344d7f40f787537dcebd4
Bernardo Braga <Bebraga12@gmail.com>
2023-12-18 13:20:19 -0300

    chessexeption, chessPosition

---
+++ src/chess/ChessExeption.java
@@ -0,0 +1,10 @@
+package chess;
+
+public class ChessExeption extends RuntimeException{
+    private static final long serialVersionUID = 1L;
+
+    public  ChessExeption(String msg){
+        super(msg);
+    }
+        
+}
