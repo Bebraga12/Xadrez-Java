e40183bb2f507a30b147b073a10d8293094f676b
Bernardo Braga <Bebraga12@gmail.com>
2023-12-20 03:30:14 -0300

    movimentos do rei adicionado

--- src/application/UI.java
+++ src/application/UI.java
@@ -68,7 +68,7 @@ public class UI {
 
 	private static void printPiece(ChessPiece piece, boolean background) {
 		if(background){
-			System.out.print(ANSI_PURPLE_BACKGROUND);
+			System.out.print(ANSI_BLUE_BACKGROUND);
 		}
 		if (piece == null) {
 			System.out.print("-" + ANSI_RESET);
