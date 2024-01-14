c0c2e722c6078c370044103723dace463aadb1b9
Bernardo Braga <Bebraga12@gmail.com>
2024-01-14 18:35:13 -0300

    roque grande

--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -77,25 +77,24 @@ public class King extends ChessPiece {
 		}
 
 		//roque
-		if(getMoveCount() == 0 && !chessMatch.getCheck()){
+		if (getMoveCount() == 0 && !chessMatch.getCheck()) {
+			// #specialmove castling kingside rook
 			Position posT1 = new Position(position.getRow(), position.getColumn() + 3);
-			if(testRookCastling(posT1)){
+			if (testRookCastling(posT1)) {
 				Position p1 = new Position(position.getRow(), position.getColumn() + 1);
 				Position p2 = new Position(position.getRow(), position.getColumn() + 2);
-				if(getBoard().piece(p1) == null && getBoard().piece(p2) == null){
+				if (getBoard().piece(p1) == null && getBoard().piece(p2) == null) {
 					mat[position.getRow()][position.getColumn() + 2] = true;
 				}
 			}
-		}
-
-		
-		Position posT2 = new Position(position.getRow(), position.getColumn() -4);
-		if(testRookCastling(posT2)){
-			Position p1 = new Position(position.getRow(), position.getColumn() - 1);
-			Position p2 = new Position(position.getRow(), position.getColumn() - 2);
-			Position p3 = new Position(position.getRow(), position.getColumn() - 3);
-			if(getBoard().piece(p1) == null && getBoard().piece(p2) == null && getBoard().piece(p3) == null){
-				mat[position.getRow()][position.getColumn() - 2] = true;
+			Position posT2 = new Position(position.getRow(), position.getColumn() - 4);
+			if (testRookCastling(posT2)) {
+				Position p1 = new Position(position.getRow(), position.getColumn() - 1);
+				Position p2 = new Position(position.getRow(), position.getColumn() - 2);
+				Position p3 = new Position(position.getRow(), position.getColumn() - 3);
+				if (getBoard().piece(p1) == null && getBoard().piece(p2) == null && getBoard().piece(p3) == null) {
+					mat[position.getRow()][position.getColumn() - 2] = true;
+				}
 			}
 		}
 		return mat;
--- src/chess/pieces/Knight.java
+++ src/chess/pieces/Knight.java
@@ -32,7 +32,7 @@ public class Knight extends ChessPiece {
 			mat[p.getRow()][p.getColumn()] = true;
 		}
 
-		p.setValues(position.getRow()-1, position.getColumn()-2);
+		p.setValues(position.getRow()-2, position.getColumn()-1);
 		if(getBoard().positionExists(p) && canMove(p)) {
 			mat[p.getRow()][p.getColumn()] = true;
 		}
@@ -47,12 +47,12 @@ public class Knight extends ChessPiece {
 			mat[p.getRow()][p.getColumn()] = true;
 		}
 		
-		p.setValues(position.getRow() +1, position.getColumn() +1);
+		p.setValues(position.getRow() +1, position.getColumn() +2);
 		if(getBoard().positionExists(p) && canMove(p)) {
 			mat[p.getRow()][p.getColumn()] = true;
 		}
 
-		p.setValues(position.getRow() +2, position.getColumn() +2);
+		p.setValues(position.getRow() +2, position.getColumn() +1);
 		if(getBoard().positionExists(p) && canMove(p)) {
 			mat[p.getRow()][p.getColumn()] = true;
 		}
